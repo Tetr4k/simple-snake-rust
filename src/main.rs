e15f6bdf3d969871cbc30bb35b176fcd5db92cd3
Gabriel Ribeiro <g2012ribeiro@gmail.com>
2025-02-28 23:14:08 -0300

    feat: rendering snake

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ const LARGURA_PADRAO: i8 = 16;
 
 type Celula = (i8, i8);
 
-const CIMA:(i8, i8)       = (0, 1);
-const BAIXO:(i8, i8)      = (0, -1);
-const ESQUERDA:(i8, i8)   = (1, 0);
-const DIREITA:(i8, i8)    = (-1, 0);
+const CIMA:(i8, i8)       = (0, -1);
+const BAIXO:(i8, i8)      = (0, 1);
+const ESQUERDA:(i8, i8)   = (-1, 0);
+const DIREITA:(i8, i8)    = (1, 0);
 
 struct Cobra {
     cabeca: Celula,
@@ -19,9 +19,15 @@ struct Cobra {
 
 impl Cobra{
     fn new(largura: i8) -> Self {
+        let mut corpo = Vec::new();
+
+        for _ in 0..3{
+            corpo.push((largura/2, largura/2));
+        }
+
         Cobra {
-            cabeca: (largura/2, largura/2),
-            corpo: Vec::new(),
+            cabeca: corpo[0],
+            corpo,
             proximo: CIMA,
         }
     }
@@ -30,7 +36,9 @@ impl Cobra{
         let (cx, cy) = self.cabeca;
         let (px, py) = self.proximo;
         let proxima: Celula = (cx + px, cy + py);
-        self.corpo.push(proxima);
+
+        self.corpo.insert(0, proxima);
+        self.cabeca = proxima;
         self.corpo.pop();
     }
 }
@@ -43,6 +51,8 @@ async fn main() {
 
     let largura_grade = args.get(1).and_then(|s| s.parse().ok()).unwrap_or(LARGURA_PADRAO);
 
+    let mut cobra = Cobra::new(largura_grade);
+
     loop {
         clear_background(GRAY);
 
@@ -51,6 +61,18 @@ async fn main() {
             let l = i as f32 * TAM_CELULA;
             draw_line(l, 0.0, l, TAM_CELULA * largura_grade as f32, 2.0, WHITE);
             draw_line(0.0, l, TAM_CELULA * largura_grade as f32, l, 2.0, WHITE);
+        }
+
+        cobra.mover();
+
+        for (x, y) in &cobra.corpo {
+            draw_rectangle(
+                *x as f32 * TAM_CELULA,
+                *y as f32 * TAM_CELULA,
+                TAM_CELULA,
+                TAM_CELULA,
+                GREEN
+            );
         }
 
         next_frame().await
